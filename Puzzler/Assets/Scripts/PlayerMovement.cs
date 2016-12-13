c76d09d2abc770a7008fbe2c24ef06aa11cce814
Olutade Ajiboye <mrbigheaddd@gmail.com>
2016-12-13 22:41:34 +0100

    feat: GUI interaction

---
+++ Puzzler/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public Transform player;
+	public Transform initialPoint;
+	public Transform finalPoint;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GoToInitialPoint()
+	{
+		StartCoroutine(Motion(player.position, initialPoint.position));
+	}
+
+	public void GoToFinalPoint()
+	{
+		StartCoroutine(Motion(player.position, finalPoint.position));
+	}
+
+	IEnumerator Motion(Vector3 iniPos, Vector3 finalPos)
+	{
+		float timeMove = 0f;
+		while (true)
+		{
+			timeMove += Time.deltaTime;
+			transform.position = Vector3.Lerp(iniPos, finalPos, timeMove);
+			if (transform.position == finalPos)
+			{
+				yield break;
+			}
+			yield return null;
+		}
+	}
+}
