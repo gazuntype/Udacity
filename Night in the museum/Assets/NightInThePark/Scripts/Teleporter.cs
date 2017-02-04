9c534614fa781feedfad4bb8be9bd3f7962a7699
Olutade Ajiboye <mrbigheaddd@gmail.com>
2017-02-04 15:29:40 +0100

    feat: teleportation complete

--- Night in the museum/Assets/NightInThePark/Scripts/Teleporter.cs
+++ Night in the museum/Assets/NightInThePark/Scripts/Teleporter.cs
@@ -16,6 +16,9 @@ public class Teleporter : MonoBehaviour {
 	[Tooltip("The height of the user")]
 	public float height = 1.75f;
 
+	[Tooltip("The height of the canvas")]
+	public float canvasHeight = 2;
+
 	int wayPointIndex = 0;
 
 	// Use this for initialization
@@ -28,12 +31,14 @@ public class Teleporter : MonoBehaviour {
 		if (UIControl.moveClicked)
 		{
 			wayPointIndex = (wayPointIndex + 1) % wayPoints.Length;
-			Vector3 canvasDestination = wayPointsCanvas[wayPointIndex].position + (Vector3.up * height);
+			Vector3 canvasDestination = wayPointsCanvas[wayPointIndex].position + (Vector3.up * canvasHeight);
 			canvas.transform.position = canvasDestination;
-			canvas.transform.Rotate(new Vector3(0, 90, 0));
+			canvas.transform.LookAt(wayPoints[wayPointIndex].position + (Vector3.up * height));
+			canvas.transform.Rotate(new Vector3(0, 180, 0));
 			UIControl.moveClicked = false;
 		}
 		Vector3 destination = wayPoints[wayPointIndex].position + (Vector3.up * height);
 		transform.position = Vector3.Lerp(transform.position, destination, Time.deltaTime * speed);
+		transform.LookAt(wayPointsCanvas[wayPointIndex].position + (Vector3.up * canvasHeight));
 	}
 }
