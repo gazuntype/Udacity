311489b16630fb83d838ff85f032973268286894
Olutade Ajiboye <mrbigheaddd@gmail.com>
2017-02-07 09:06:03 +0100

    feat: created imageControl script

---
+++ Night in the museum/Assets/NightInThePark/Scripts/ImageControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ImageControl : MonoBehaviour {
+	[Tooltip("All the images that can be displayed")]
+	public Sprite[] images;
+
+	[Tooltip("Image Objects that display the images")]
+	public Image[] imageObjects;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
