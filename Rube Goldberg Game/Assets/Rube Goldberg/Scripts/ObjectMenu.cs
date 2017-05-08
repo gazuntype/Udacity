24aaaeee72389ed082ef2d68cb8a21eaf1c672f3
Olutade Ajiboye <mrbigheaddd@gmail.com>
2017-05-08 11:58:18 +0100

    feat: began work on object menu

---
+++ Rube Goldberg Game/Assets/Rube Goldberg/Scripts/ObjectMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMenu : MonoBehaviour {
+
+	private SteamVR_TrackedObject trackedObject;
+	private SteamVR_Controller.Device VRDevice;
+
+	// Use this for initialization
+	void Start () {
+		trackedObject = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		VRDevice = SteamVR_Controller.Input((int)trackedObject.index);
+	}
+}
