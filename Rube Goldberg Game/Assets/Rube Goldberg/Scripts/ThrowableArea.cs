47e316f1606f72ec8adb65e9284a56e7eddbc44c
Olutade Ajiboye <mrbigheaddd@gmail.com>
2017-05-10 09:27:10 +0100

    feat: created throwing platform

---
+++ Rube Goldberg Game/Assets/Rube Goldberg/Scripts/ThrowableArea.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableArea : MonoBehaviour {
+
+	private Renderer areaRenderer;
+	private Material areaMaterial;
+	// Use this for initialization
+	void Start () {
+		areaRenderer = GetComponent<Renderer>();
+		areaMaterial = areaRenderer.material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			
+			areaMaterial.color = Color.green;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			Debug.Log("Player left");
+			areaMaterial.color = Color.red;
+		}
+	}
+}
